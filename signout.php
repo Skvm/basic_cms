cb891bf2addb256da1b01e2d94d70eb32b130d89
Skam <info@skam.me>
2016-09-18 02:18:51 +0100

    fixed signout

--- signout.php
+++ signout.php
@@ -1,25 +1,17 @@
 <?php
-//signout.php
 include 'connect.php';
-include 'header.php';
 
 echo '<h2>Sign out</h2>';
 
-//check if user if signed in
 if(isset($_SESSION['signed_in']))
 {
-	//unset all variables
-	$_SESSION['signed_in'] = NULL;
-	$_SESSION['user_name'] = NULL;
-	$_SESSION['user_id']   = NULL;
 	session_destroy();
 	
 	echo 'Succesfully signed out, thank you for visiting.';
 }
 else
 {
-	echo 'You are not signed in. Would you <a href="signin.php">like to</a>?';
+	echo 'You are not signed in.';
 }
 
-include 'footer.php';
 ?>
